ae76914d67479cc4e398d5e29306c068234bd56b
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-03 07:21:45 -0500

    Moved module includes into an inline script tag for quicker loading.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,2 @@
-import JackImage from '/elements/image.js';
-
 document.addEventListener('DOMContentLoaded', () =>
   document.querySelector('body').style.paddingBottom = document.querySelector('.footer').offsetHeight + 'px');
