bfe1851e33676c4beaf98c4d34e88a6855e4f591
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-03 06:32:07 -0500

    Some basic logic to build an image url

--- src/elements/image.js
+++ src/elements/image.js
@@ -1,7 +1,18 @@
 export default class JackImage extends HTMLElement {
   constructor() {
     super();
-    console.log('Hello, World!');
+    console.log('Hello, World!', this);
+
+    const shadowRoot = this.attachShadow({mode: 'open'});
+
+    shadowRoot.innerHTML = `
+      <style>
+        img {
+          max-width: 100%;
+        }
+      </style>
+      <img src="/assets/images/4_3/${this.getAttribute('image')}.jpg">
+    `;
   }
 }
 
